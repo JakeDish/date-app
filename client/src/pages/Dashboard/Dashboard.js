9c0912030dd0d06fd05440aad814e6ae47e4c946
Eric <ericnguyen23@gmail.com>
2023-05-01 13:44:48 -0700

    Added logged in users info

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -2,13 +2,26 @@ import Grid from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Header from "../../components/Header";
 
+import { useQuery } from "@apollo/client";
+import { QUERY_ME } from "../../utils/queries";
+
 function Dashboard() {
+  const { data } = useQuery(QUERY_ME);
+  let user;
+
+  if (data) {
+    user = data.me;
+  }
+
   return (
     <Grid container spacing={2}>
       <Grid display="flex" justifyContent="center" alignItems="center">
         <Header />
-        <Typography variant="h3"> Your Dashboard</Typography>
-        {/* dynamically render */}
+        {user ? (
+          <Typography variant="h3"> {user.name}'s Dashboard</Typography>
+        ) : (
+          <Typography variant="h3"> No User Defined</Typography>
+        )}
       </Grid>
     </Grid>
   );
