b989a50f2c217f8db10f2fca31487bf57c10fa20
Sm3gma <gabriel819.gl@gmail.com>
2023-05-02 11:49:45 -0700

    created cards

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -6,26 +6,41 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
+const CardContainer = { border: "1px solid red" };
+
 export default function ProfileCard(props) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={props.image}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+    <div style={{ border: "1px solid gray", borderRadius:"8px", margin: "20px 0", display: "flex" }}>
+      <div style={{padding: "40px" }}>
+        <img src={props.image} alt="profileImage" style={{border: "1px solid black", borderRadius: "8px"}} />{" "}
+      </div>
+      <div style={{padding: "40px" }}>
+        <Typography gutterBottom variant="h4" component="div">
           {props.name}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary" style={{fontSize: "24px"}}>
           {props.bio}
         </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">View</Button>
-      </CardActions>
-    </Card>
+      </div>
+    </div>
+    // <Card sx={{ maxWidth: 345 }}>
+    //   <CardMedia
+    //     component="img"
+    //     alt="green iguana"
+    //     height="140"
+    //     image={props.image}
+    //   />
+    //   <CardContent>
+    //     <Typography gutterBottom variant="h5" component="div">
+    //       {props.name}
+    //     </Typography>
+    //     <Typography variant="body2" color="text.secondary">
+    //       {props.bio}
+    //     </Typography>
+    //   </CardContent>
+    //   <CardActions>
+    //     <Button size="small">View</Button>
+    //   </CardActions>
+    // </Card>
   );
 }
