cef4eae71365110363f4d5addc79830104ffd1e4
Eric <ericnguyen23@gmail.com>
2023-05-04 15:29:39 -0700

    Added hero

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import Typography from "@mui/material/Typography";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
@@ -8,8 +9,21 @@ import Button from "@mui/material/Button";
 import { useQuery } from "@apollo/client";
 import { QUERY_ME } from "../../utils/queries";
 import { QUERY_USERS } from "../../utils/queries";
+import Couple from "../../img/pexels-photo-1405739.webp";
+
+const Hero = styled.header`
+  height: 60vh;
+  background ${`linear-gradient(0deg, rgba(40, 11, 128, 0.8),rgba(40, 11, 128, 0.8)),url(${Couple}) no-repeat center cover;`}
+`;
 
 const styles = {
+  heroInner: {
+    position: "relative",
+    height: "100%",
+  },
+  heroText: {
+    paddingTop: "12%",
+  },
   links: {
     color: "black",
     marginRight: "10px",
@@ -33,30 +47,37 @@ function Home() {
   return (
     <div>
       <Header />
+      <Hero>
+        <div style={styles.heroInner}>
+          <div style={styles.heroText}>
+            {user ? (
+              <Typography variant="h4" textAlign="center">
+                {user.name}, Welcome to MatchMaker.
+                <br />
+                Please browse to find your match!
+              </Typography>
+            ) : (
+              <Typography variant="h4" textAlign="center">
+                <div style={{ marginBottom: "24px" }}>
+                  Welcome to MatchMaker! <br />
+                  Sign up today to see your matches!
+                </div>
+                <Link to="/register" style={styles.links}>
+                  <Button size="large" color="primary" variant="outlined">
+                    SIGN UP!
+                  </Button>
+                </Link>
+                <Link to="/login" style={styles.links}>
+                  <Button size="large" color="primary" variant="outlined">
+                    Login
+                  </Button>
+                </Link>
+              </Typography>
+            )}
+          </div>
+        </div>
+      </Hero>
       <Container>
-        {user ? (
-          <Typography variant="h4" textAlign="center">
-            {user.name}, Welcome to MatchMaker.
-            <br />
-            Please browse to find your match!
-          </Typography>
-        ) : (
-          <Typography variant="h4" textAlign="center">
-            Welcome to MatchMaker! <br />
-            Sign up today to see your matches!
-            <br />
-            <Link to="/register" style={styles.links}>
-              <Button size="large" color="primary" variant="outlined">
-                SIGN UP!
-              </Button>
-            </Link>
-            <Link to="/login" style={styles.links}>
-              <Button size="large" color="primary" variant="outlined">
-                Login
-              </Button>
-            </Link>
-          </Typography>
-        )}
         {/* Render all users in ProfileCard component */}
         {allusers
           ? allusers.map((singleUser) => {
